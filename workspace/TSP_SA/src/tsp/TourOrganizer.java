3e42c469cac28db0f7e1eac313565f62a592e1af
Mustafa Arikan <mustafa@inode.at>
2015-01-02 11:36:50 +0100

    Finish TourOrganizer

--- workspace/TSP_SA/src/tsp/TourOrganizer.java
+++ workspace/TSP_SA/src/tsp/TourOrganizer.java
@@ -1,5 +1,25 @@
 package tsp;
 
+import java.util.ArrayList;
+
 public class TourOrganizer {
+	
+    // Holds our nodes
+    private static ArrayList<Node> destinationNodes = new ArrayList<Node>();
+
+    // Adds a destination node
+    public static void addCity(Node node) {
+        destinationNodes.add(node);
+    }
+    
+    // Get a node
+    public static Node getCity(int index){
+        return (Node)destinationNodes.get(index);
+    }
+    
+    // Get the number of destination nodes
+    public static int numberOfCities(){
+        return destinationNodes.size();
+    }
 
 }
